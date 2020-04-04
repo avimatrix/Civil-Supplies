e7c4390414ec0c38f3fcb334be9c5f3b7685319f
Avinash Kondeti <kondetiavinashbalaji@gmail.com>
2020-04-04 15:00:10 +0530

    adding bootstrap and angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,11 @@ import { CreateIndentComponent } from './Dashboard/create-indent/create-indent.c
 import { ManageIndentComponent } from './Dashboard/manage-indent/manage-indent.component';
 import { CreateDispatchComponent } from './Dashboard/create-dispatch/create-dispatch.component';
 import { ArrivalsComponent } from './Dashboard/arrivals/arrivals.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, LoginComponent, DashboardComponent, CreateIndentComponent, ManageIndentComponent, CreateDispatchComponent, ArrivalsComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
