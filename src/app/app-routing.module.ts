6b43cb166c9295f009e905c068dd85c12f4bf372
Avinash Kondeti <kondetiavinashbalaji@gmail.com>
2020-04-08 23:03:21 +0530

    adding dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { LoginComponent } from "./login/login.component";
+import { DashboardComponent } from './Dashboard/dashboard/dashboard.component';
 
-const routes: Routes = [{ path: "/", component: LoginComponent }];
+const routes: Routes = [{ path: "", component: DashboardComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
